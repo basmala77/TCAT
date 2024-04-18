dd946c4d9094dc9cf6d809ee61dd461cb0fffa1a
basmala77 <basmalaelshabrawy37@gmail.com>
2024-04-18 15:19:46 +0200

    c# projects

---
+++ TaskCat6/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine(await FetchData());
+            Console.ReadLine();
+        }
+        static async Task<string> FetchData()
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync("https://icy-archduke-e8e.notion.site/NET-Test-4e92251f464a47dea170154e897a2872");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
